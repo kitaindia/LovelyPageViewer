7b05161c0649a08c8ddf6ca3507dfc6e1436833c
Masaki Takehara <morisot64@gmail.com>
2018-05-06 17:24:41 +0900

    Initial Commit

---
+++ LovelyPageViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LovelyPageViewer
+//
+//  Created by 竹原正起 on 2018/05/06.
+//  Copyright © 2018年 masaki.takehara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
